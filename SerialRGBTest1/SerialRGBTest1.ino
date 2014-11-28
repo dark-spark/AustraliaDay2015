797807235689d34b7a99f2d2454125d7f21e8589
dark-spark <matt.syme@gmail.com>
2014-11-28 21:14:46 +1000

    started integration with new authorised http connection. Also added to do list

---
+++ SerialRGBTest1/SerialRGBTest1.ino
@@ -0,0 +1,106 @@
+boolean red, green, blue, white;
+boolean falling_edge_trigger17 = false;
+boolean falling_edge_trigger13 = false;
+boolean stringComplete = false;
+String inputString = "";
+
+void setup() {
+  Serial.begin(9600);
+
+  pinMode(12, OUTPUT);
+  pinMode(14, OUTPUT);
+  pinMode(15, OUTPUT);
+  pinMode(16, OUTPUT);
+  pinMode(17, INPUT_PULLUP);
+  pinMode(13, INPUT);
+  digitalWrite(12, true);
+  digitalWrite(15, true);
+  digitalWrite(16, true);
+  digitalWrite(14, true);
+  delay(1000);
+}
+
+void loop() {
+
+  if (stringComplete) {
+    Serial.println(inputString);
+    if (inputString == "redON") {
+      //      Serial.println("Red ON");
+      red = true;
+    } 
+    else if (inputString == "redOFF") {
+      //      Serial.println("Red OFF");
+      red = false;
+    } 
+    else if (inputString == "greenON") {
+      //      Serial.println("Green ON");
+      green = true;
+    } 
+    else if (inputString == "greenOFF") {
+      //      Serial.println("Green OFF");
+      green = false;
+    } 
+    else if (inputString == "blueON") {
+      //      Serial.println("Blue ON");
+      blue = true;
+    } 
+    else if (inputString == "blueOFF") {
+      //      Serial.println("Blue OFF");
+      blue = false;
+    } 
+    else if (inputString == "yellowON") {
+      //      Serial.println("Yellow ON");
+      white = true;
+    } 
+    else if (inputString == "yellowOFF") {
+      //      Serial.println("Yellow OFF");
+      white = false;
+    }
+    stringComplete = false;
+    inputString = "";
+  }
+  digitalWrite(14, red);
+  digitalWrite(15, green);
+  digitalWrite(16, blue);
+  //  digitalWrite(12, white);
+
+  if (digitalRead(17) == LOW) {
+    if (falling_edge_trigger17 == false) {
+      Serial.println("t 100");
+      falling_edge_trigger17 = true;
+      delay(10);
+    }
+  }
+  if (digitalRead(17) == HIGH) {
+    falling_edge_trigger17 = false;
+    delay(10);
+  }  
+  if (digitalRead(13) == LOW) {
+    digitalWrite(12, HIGH);
+    //    if (falling_edge_trigger13 == false) {
+    //      Serial.println("t jump");
+    //      falling_edge_trigger13 = true;
+    //      delay(10);
+    //    }
+    //  }
+    //  if (digitalRead(13) == HIGH) {
+    //    falling_edge_trigger13 = false;
+    //    delay(10);
+  } 
+  else {
+    digitalWrite(12, LOW);
+  }
+
+  while (Serial.available()) {
+    char inChar = (char)Serial.read(); 
+    if (inChar == '.') {
+      stringComplete = true;
+      break;
+    } 
+    inputString += inChar;
+  }
+}
+
+
+
+
